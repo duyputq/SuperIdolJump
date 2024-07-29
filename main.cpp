f8d57aaebdb4a5523828e624d14fcc8eed0947a9
Duy <duy20nguyen04@gmail.com>
2024-07-29 10:26:14 +0700

    update main

--- main.cpp
+++ main.cpp
@@ -4,14 +4,36 @@ Game* game = nullptr;
 
 int main(int argc, char* argv[])
 {
-	game = new Game();
-	game->init("GameWindow", 800, 600, false);
+	/*Limit Rate Frame*/
+	const int FPS = 60;
+	const int frameDelay = 1000 / FPS;
 
+	Uint32 frameStart;
+	int frameTime;
+
+	/*khoi tao*/
+	game = new Game();
+	game->init("GameWindow", 800, 640, false);
+
+	/*cho game vao vong lap*/
 	while (game->running())
 	{
+		frameStart = SDL_GetTicks();
+
+		/*xu ly su kien*/
 		game->handleEvents();
+
+		/*update*/
 		game->update();
 		game->render();
+
+		frameTime = SDL_GetTicks() - frameStart;
+
+		/*tao cai delay nay de limit frame*/
+		if (frameDelay > frameTime)
+		{
+			SDL_Delay(frameDelay - frameTime);
+		}
 	}
 
 	game->clean();

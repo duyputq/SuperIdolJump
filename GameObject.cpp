4a80f57cb7b3c075a88eaefb8afb666f0bb9b3db
Duy <duy20nguyen04@gmail.com>
2024-07-29 10:22:05 +0700

    update map, chracters

---
+++ GameObject.cpp
@@ -0,0 +1,40 @@
+#include "GameObject.h"
+#include "TextureManager.h"
+#include <iostream>
+using namespace std;
+int cnt = 0;
+
+
+GameObject::GameObject(const char* texturesheet, int x, int y)
+{
+	objTexture = TextureManager::LoadTexture(texturesheet);
+	xpos = x;
+	ypos = y;
+}
+
+void GameObject::Update()
+{
+
+	xpos++;
+	ypos++;
+
+	srcRect.h = 32;
+	srcRect.w = 32;
+	srcRect.x = 0;
+	srcRect.y = 0;
+
+	destRect.x = xpos;
+	destRect.y = ypos;
+	destRect.w = srcRect.w * 2;
+	destRect.h = srcRect.h * 2;
+
+	cnt++;
+	cout << cnt << endl;
+
+
+}
+
+void GameObject::Render()
+{
+	SDL_RenderCopy(Game::renderer, objTexture, &srcRect, &destRect);
+}
---
+++ GameObject.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Game.h"
+
+class GameObject {
+
+public:
+	GameObject(const char* texture, int x, int y);
+	~GameObject();
+
+	void Update();
+	void Render();
+private:
+	int xpos;
+	int ypos;
+
+	SDL_Rect srcRect, destRect;
+	SDL_Texture* objTexture;
+	//SDL_Renderer* renderer;	
+};
